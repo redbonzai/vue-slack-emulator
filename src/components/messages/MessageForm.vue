9a25a26608427c3296d2825636cf984ef49ea85f
Christian Dominguez <christian@redbonzai.com>
2018-11-27 20:36:16 -0600

    MessagingComponents: Created Messaging structure in Messages , and form styling in MessageForm

--- src/components/messages/MessageForm.vue
+++ src/components/messages/MessageForm.vue
@@ -1,13 +1,93 @@
 <template>
   <div>
-    <form>
-      Message Form
-    </form>
+    <div class="messageForm">
+        <form>
+            <div class="input-group mb-3">
+                <input
+                    v-model.trim="message"
+                    name="message"
+                    id="message"
+                    placeholder="write a message"
+                    class="form-control mt-3" autofocus>
+
+                <div class="input-group-append">
+                    <button @click="sendMessage" class="btn btn-primary mt-3" type="button"> &nbsp;Send</button>
+                </div>
+
+                <div class="input-group-append">
+                    <button class="btn btn-warning mt-3" type="button">Upload</button>
+                </div>
+            </div>
+        </form>
+    </div>
   </div>
 </template>
 
 <script>
+    import {mapGetters} from 'vuex'
+
     export default {
-      name: 'message-form'
+      name: 'message-form',
+        data() {
+          return {
+              message: '',
+              errors: []
+          }
+        },
+
+        computed: {
+            ...mapGetters(['currentChannel', 'currentUser'])
+        },
+
+        methods: {
+            sendMessage() {
+
+                //construct a new message object.
+                let newMessage = {
+                    content: this.message,
+                    timestamp: firebase.database.ServerValue.TIMESTAMP,
+                    user: {
+                        name: this.currentUser.displayName,
+                        avatar: this.currentUser.photoURL,
+                        id: this.currentUser.uid
+                    }
+                };
+
+                if (this.currentChannel !== null) {
+                    if (this.message.length > 0) {
+                        this.$parent.messagesRef.child(this.currentChannel.id).push().set(newMessage)
+                            .then( () => {
+                                //
+                            })
+                            .catch( (error) => {
+                                this.errors.push(error.message);
+                            });
+                        this.message = '';
+                    }
+                }
+            }
+        }
     }
 </script>
+
+<style scoped>
+    .messageForm {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        z-index: 100;
+        color: white;
+        width: 61%;
+        text-align: center;
+        margin-bottom: -16px;
+        margin-left: 33.3%;
+    }
+
+    .input-group {
+        width: 100%;
+    }
+
+    input, button {
+        height: 50px;
+    }
+</style>
--- src/components/messages/Messages.vue
+++ src/components/messages/Messages.vue
@@ -1,20 +1,29 @@
 <template>
-  <div>
-      <!-- show single message -->
-      <single-message></single-message>
+    <div>
+        <!-- show single message -->
+        <single-message></single-message>
 
-      <!-- message form -->
-      <message-form></message-form>
-  </div>
+        <!-- message form -->
+        <message-form></message-form>
+    </div>
 </template>
 
 <script>
-  import SingleMessage from './SingleMessage'
-  import MessageForm from './MessageForm'
+    import SingleMessage from './SingleMessage'
+    import MessageForm from './MessageForm'
+    import database from 'firebase/database'
 
-  export default {
-    name: 'messages',
+    export default {
+        name: 'messages',
 
-    components: {SingleMessage, MessageForm}
-  }
+        components: {SingleMessage, MessageForm},
+
+        data() {
+            return {
+                messagesRef: firebase.database().ref('messages')
+            }
+        }
+
+
+    }
 </script>
